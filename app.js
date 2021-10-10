4bf8713cb2514a1a84020b42390847559909273a
atjohnfeng <atjohnfeng@gmail.com>
2021-10-10 15:15:28 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+const mongoose = require("mongoose");
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("hello"));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
