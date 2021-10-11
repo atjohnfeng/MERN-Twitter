f27f41bbb53df1af2107f0e54dbaf03c5d7128a9
atjohnfeng <atjohnfeng@gmail.com>
2021-10-10 22:30:22 -0700

    add frontend

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -2,5 +2,41 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/test", (req, res) => res.json({ msg: "This is the tweets route." }));
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Tweet = require('../../models/Tweet');
+const validateTweetInput = require('../../validation/tweets');
+
+router.get('/', (req, res) => {
+    Tweet.find()
+        .sort({date: -1})
+        .then(tweets => res.json(tweets))
+        .catch(err => res.status(404).json({ notweetsfound: 'No tweers were found'}))
+})
+
+router.get('/user/:user_id', (req, res) => {
+    Tweet.find({user: req.params.user_id})
+        .then(tweets => res.json(tweets))
+        .catch(err =>
+            res.status(404).json({ notweetsfound: 'This user has no tweets' }))
+})
+
+router.post('/',
+    passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+        const {errors, isValid} = validateTweetInput(req.body);
+
+        if(!isValid) {
+            return res.status(400).json(errors);
+        }
+
+        const newTweet = new Tweet({
+            text: req.body.text,
+            user: req.user.id
+        })
+
+        newTweet.save().then(tweet => res.json(tweet));
+    })
 
 module.exports = router;
